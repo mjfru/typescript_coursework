1c4b1d5d12603453d5bdef8ced08a17d1d9e61b5
Matt Frutchey <mjfru201@gmail.com>
2024-05-30 08:45:39 -0400

    finish type declaration notes

---
+++ ts_notes/17type_declarations.ts
@@ -0,0 +1,9 @@
+/*
+! Type Declarations - Working w/ 3rd Party Code
+* Files that end with .d.ts we'll find files that have ONLY type information; they don't produce .js outputs.
+* There is no logic in these files, just interfaces, variables, objects, etc.
+
+
+! Working with Axios
+
+*/
